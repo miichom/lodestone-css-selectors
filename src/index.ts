a892d46905e798574cd501c24aa652ccdc75c6d5
miichom <hello@cammy.xyz>
2026-08-23 09:50:02 +0100

    chore: restructure schemas

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export * as character from "./lib/schemas/character.ts";
-export * as cwls from "./lib/schemas/cwls.ts";
-export * as freecompany from "./lib/schemas/freecompany.ts";
-export * as linkshell from "./lib/schemas/linkshell.ts";
-export * as metadata from "./lib/schemas/metadata.ts";
-export * as pvpteam from "./lib/schemas/pvpteam.ts";
+export * as character from "./schemas/character.ts";
+export * as cwls from "./schemas/cwls.ts";
+export * as freecompany from "./schemas/freecompany.ts";
+export * as linkshell from "./schemas/linkshell.ts";
+export * as metadata from "./schemas/metadata.ts";
+export * as pvpteam from "./schemas/pvpteam.ts";
